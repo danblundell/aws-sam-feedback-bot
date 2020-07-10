dbd77924b8c89bf1d39c9d2a4fae2fa4ac46c124
Dan Blundell <hello@danblundell.com>
2020-07-10 16:19:58 +0100

    adds default fallback for test suite to run

--- src/validation/dispatch.js
+++ src/validation/dispatch.js
@@ -6,7 +6,7 @@ module.exports = async (intentRequest) => {
     console.log(`dispatch userId=${intentRequest.userId}, intentName=${intentRequest.currentIntent.name}`);
     console.log(intentRequest);
     const intentName = intentRequest.currentIntent.name;
-    const deployedIntent = process.env.feedbackIntentName;
+    const deployedIntent = process.env.feedbackIntentName || 'captureBotFeedback';
 
     if (intentName === deployedIntent){
         console.log(intentName + ' was called');
