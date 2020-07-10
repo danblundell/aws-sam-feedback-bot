f31be45d30b7d58d426173a7d1d0b0cc4ed4bd41
Dan Blundell <hello@danblundell.com>
2020-07-10 15:18:15 +0100

    updates lambda code to use dynamic intent name

--- src/validation/dispatch.js
+++ src/validation/dispatch.js
@@ -6,8 +6,9 @@ module.exports = async (intentRequest) => {
     console.log(`dispatch userId=${intentRequest.userId}, intentName=${intentRequest.currentIntent.name}`);
     console.log(intentRequest);
     const intentName = intentRequest.currentIntent.name;
+    const deployedIntent = process.env.feedbackIntentName;
 
-    if (intentName === 'captureBotFeedback'){
+    if (intentName === deployedIntent){
         console.log(intentName + ' was called');
         return await getService(intentRequest);
     } else {
